3f2437eb4edc2bfc04f4ce4b04adaa5dffb88c68
jamextech1 <okorojameschizaram@gmail.com>
2022-11-30 23:48:34 +0100

    ci/cd

--- js/app.js
+++
@@ -1,5 +0,0 @@
-const navCta = document.querySelector(".get-started");
-const btn = document.querySelector(".hamburger-open");
-btn.addEventListener("click", function (e) {
-  btn.style = "padding-right: -90rem;";
-});
