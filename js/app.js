d2fd5ba8da904666f3bda5b14342cb49b17d5581
jamextech1 <okorojameschizaram@gmail.com>
2022-12-04 13:14:38 +0100

    ci/cd

--- js/app.js
+++ js/app.js
@@ -67,3 +67,25 @@ $(".counter").counterUp({
   delay: 1,
   time: 300,
 });
+
+// BACK TO TOP BTN:
+let mybutton = document.querySelector(".back-to-top-icon");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
